4320ea359bf7f949bd7e601e1b92d5eef141c575
Jangoo505 <desai.jainesh@gmail.com>
2022-05-19 16:04:53 -0300

    this is final commit

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+import VueMeta from 'vue-meta'
+Vue.use(VueMeta)
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
