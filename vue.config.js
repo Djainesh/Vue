2e19a6580a12a6c006183cdf27b517e50b6a12ee
Jainesh Desai <desai.jainesh@gmail.com>
2025-03-21 18:32:08 -0400

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,9 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+- name: Manually Install Maven
+  run: |
+    curl -fsSL https://dlcdn.apache.org/maven/maven-3/3.8.8/binaries/apache-maven-3.8.8-bin.tar.gz -o maven.tar.gz
+    tar -xzf maven.tar.gz
+    mv apache-maven-3.8.8 $HOME/maven
+    echo "$HOME/maven/bin" >> $GITHUB_PATH
